80880dbf4fe4f3c123c10e9f9111b5bb7d8884e6
Pedro Macias <pin.macias@gmail.com>
2020-12-31 11:12:20 +0100

    primeras pruebas con las bases de datos

--- forms.py
+++ forms.py
@@ -1,15 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, TextAreaField
+from wtforms import StringField, SubmitField, PasswordField, TextAreaField, BooleanField
 from wtforms.validators import DataRequired, Email, Length
 
 class FormularioRegistro(FlaskForm):
-    nombre = StringField('Nombre', validators=[DataRequired(), Length(20)])
+    nombre = StringField('Nombre', validators=[DataRequired(), Length(min=5, max=20)])
     email = StringField('Email', validators=[DataRequired(),Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit= SubmitField('Registrar')
 
 class FormularioPost(FlaskForm):
-    titulo = StringField('Titulo', validators=[DataRequired(), Length(20)])
-    sub_titulo = StringField('Subtitulo', validators=[DataRequired(),Length(40)])
+    titulo = StringField('Titulo', validators=[DataRequired(), Length(min=10, max=50)])
     contenido= TextAreaField('Contenido')
-    submit= SubmitField('Enviar')    
+    submit= SubmitField('Enviar')
+
+class FormularioLogin(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Recuérdame')
+    submit = SubmitField('Login')    
